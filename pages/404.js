8c71d45ffb588ec36c50b9a32004908f7d055a0d
Catzzz <chuneleu@gmail.com>
2021-07-09 09:46:03 -0700

    add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Layout from "@/components/layout/Layout"
+import styles from '@/styles/notfound.module.scss'
+import Link from 'next/link'
+const NotFound = () => {
+    return (
+        <Layout>
+            <section className={styles.container}>
+            <h1>404 </h1>
+            <p>Something wrong</p>
+            </section>
+        </Layout>
+    )
+}
+
+export default NotFound

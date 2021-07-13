0c8ddb39a3dc7a6fda52b4fc2708ca42b5c421a3
Catzzz <chuneleu@gmail.com>
2021-07-13 10:58:01 -0700

    remove FaExclamationTriangle in 404 page

--- pages/404.js
+++ pages/404.js
@@ -2,12 +2,12 @@ import Layout from "@/components/layout/Layout"
 import styles from '@/styles/notfound.module.scss'
 import Footer from "@/components/footer/Footer"
 import Link from 'next/link'
-import {FaExclamationTriangle} from 'react-icons/fa'
+
 const NotFound = () => {
     return (
         <Layout>
             <section className={styles.container}>
-            <h1><FaExclamationTriangle/> 404</h1>
+            <h1>404</h1>
             <p>Sorry, there is nothing here</p>
             <Link href="/"><a>Go Back Home</a></Link>
             </section>
